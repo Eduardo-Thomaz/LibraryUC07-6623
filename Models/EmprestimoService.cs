d33567f68f8ce13ca456c9cd7e3bd831b5699df3
Eduardo Thomaz <thomazeduuardo@gmail.com>
2022-07-07 13:25:28 -0300

    Ordenação da lista de empréstimos por data de devolução adicionada. Filtro por título de livro funcional, por usuário não.

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -30,11 +30,40 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)  
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                bc.Emprestimos.Include(e => e.Livro).ToList();
+
+                IQueryable<Emprestimo> query;
+                
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "NomeUsuario":
+                            query = bc.Emprestimos.Where(l => l.NomeUsuario.Contains(filtro.Filtro));
+                        break;
+
+                        case "Livro":
+                            query = bc.Emprestimos.Where(l => l.Livro.Titulo.Contains(filtro.Filtro));
+                        break;
+
+                        default:
+                            query = bc.Emprestimos;
+                        break;
+                    }
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    query = bc.Emprestimos;
+                }
+                
+                //ordenação padrão
+                return query.OrderBy(l => l.DataDevolucao).ToList();
             }
         }
 
